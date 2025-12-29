48ec6a5bf89fce0bfd4130f8b3907d7767d37c0b
AnnaVitry <anne-marion119@hotmail.com>
2025-12-29 15:30:38 +0400

    Modification des infos personnal.ts

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -4,7 +4,7 @@ export const PERSONAL_INFO = {
     description: "Hi! I'm Anna Vitry, a Full Stack Developer and Integrator. With a solid foundation from Epitech in web and mobile development, I am currently specializing as an AI Developer-Integrator at Simplon. With 3 years of experience, I bridge the gap between robust traditional architectures and AI-driven innovation, building intelligent and scalable digital solutions that meet tomorrow's technical challenges",
     journey: "My journey started with a curiosity for how things work under the hood, leading me to master modern technologies like Astro, React, and Node.js.",
     skills: ["🚀 Fast Learner", "🎢​ Problem Solver", "🛼​ Creative"],
-    heroDescription: "Full Stack Developer & AI Integration Apprentice.\n I leverage 3 years of experience in web architecture to build applications enhanced by Artificial Intelligence. From core development to training and deploying AI models, I focus on creating scalable, automated, and intelligent digital products.",
+    heroDescription: "Full Stack Developer & AI Integration Apprentice.\n I leverage 3 years of experience in web architecture to build applications enhanced by Artificial Intelligence.",
     resumeUrl: "https://drive.google.com/file/d/1LiUP0DgrTOBrsomdMS5sxPeHKSRblyIZ/view?usp=sharing",
     socialLinks: [
         {
