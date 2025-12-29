30d607b58474a2c5792a4b4ee3d11c11ef2aff86
AnnaVitry <anne-marion119@hotmail.com>
2025-12-29 15:51:37 +0400

    Modifications du Journey

--- src/data/personal.ts
+++ src/data/personal.ts
@@ -1,8 +1,8 @@
 export const PERSONAL_INFO = {
     name: "Anna Vitry",
     title: "Who stands behind the code?",
-    description: "Hi! I'm Anna Vitry, a Full Stack Developer and Integrator. With a solid foundation from Epitech in web and mobile development, I am currently specializing as an AI Developer-Integrator at Simplon. With 3 years of experience, I bridge the gap between robust traditional architectures and AI-driven innovation, building intelligent and scalable digital solutions that meet tomorrow's technical challenges",
-    journey: "My journey started with a curiosity for how things work under the hood, leading me to master modern technologies like Astro, React, and Node.js.",
+    description: "a Full Stack Developer and Integrator. With a solid foundation from Epitech in web and mobile development, I am currently specializing as an AI Developer-Integrator at Simplon. With 3 years of experience, I bridge the gap between robust traditional architectures and AI-driven innovation, building intelligent and scalable digital solutions that meet tomorrow's technical challenges",
+    journey: "My journey is driven by a relentless curiosity to understand what happens under the hood. It began with a Master’s degree in Visual Arts before transitioning into engineering to master languages like Python, PHP, and SQL. Today, I apply this analytical mindset to the deep mechanics of AI, blending my years of development experience  with the latest in model training.",
     skills: ["🚀 Fast Learner", "🎢​ Problem Solver", "🛼​ Creative"],
     heroDescription: "Full Stack Developer & AI Integration Apprentice.\n I leverage 3 years of experience in web architecture to build applications enhanced by Artificial Intelligence.",
     resumeUrl: "https://drive.google.com/file/d/1LiUP0DgrTOBrsomdMS5sxPeHKSRblyIZ/view?usp=sharing",
